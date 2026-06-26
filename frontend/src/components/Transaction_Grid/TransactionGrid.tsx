8c1b60d9875062a7d40ddcb89c0c6ffa59514e72
Henrique Silva <miguelhenrique4922@gmail.com>
2026-06-26 19:24:06 -0300

    mudando cores e icones

--- frontend/src/components/Transaction_Grid/TransactionGrid.tsx
+++ frontend/src/components/Transaction_Grid/TransactionGrid.tsx
@@ -1,6 +1,8 @@
 import { formatDate } from "@/Utils/formatDate";
 import upArrow from "../../assets/up-arrow-icon.svg";
 import downArrow from "../../assets/down-arrow-icon.svg";
+import editPen from "../../assets/edit-pen-icon.svg";
+import trash from "../../assets/trash-icon.svg";
 import { Button } from "../ui/button";
 
 type Transaction = {
@@ -21,14 +23,15 @@ type TransactionGridProps = {
 export function TransactionGrid({transactionProps}:TransactionGridProps) {
   
     const formattedDate = formatDate(transactionProps.tran_date);
-    let color = transactionProps.type == "Receita" ? "#013024" : "#501212";
+    let color = transactionProps.type == "Receita" ? "#0C1D1D" : "#1D1218";
+    let arrowBgColor = transactionProps.type == "Receita" ? "#0F402C" : "#552021";
 
   return (
-    <div className={`bg-[${color}] flex gap-5 p-5 rounded-xl items-center justify-between text-white shadow-2xl cursor-pointer`}>
+    <div className={`flex gap-5 p-5 rounded-xl items-center justify-between text-white shadow-2xl cursor-pointer`} style={{ backgroundColor: color }}>
         <div className="flex gap-5">
             <div>
                 <p className="flex items-center gap-2">
-                    <span>
+                    <span className="p-1 rounded-full" style={{ backgroundColor: arrowBgColor }}>
                         <img src={transactionProps.type == "Receita" ? upArrow : downArrow} alt="arrow icon" />
                     </span>
                     {transactionProps.value}
@@ -40,8 +43,18 @@ export function TransactionGrid({transactionProps}:TransactionGridProps) {
         </div>        
         <div className="flex items-center gap-3">
             <div className="flex items-center gap-2">
-                <Button className="cursor-pointer">Editar</Button>
-                <Button className="cursor-pointer">Excluir</Button>
+                <Button className="cursor-pointer flex items-center border-2">
+                    <span>
+                        <img src={editPen} alt="" className="w-[20px]"/>
+                    </span>
+                    Editar
+                </Button>
+                <Button className="cursor-pointer flex items-center">
+                    <span>
+                        <img src={trash} alt="" className="w-[25px]"/>
+                    </span>
+                    Excluir
+                </Button>
             </div>
             <p>{formattedDate}</p>
         </div>
